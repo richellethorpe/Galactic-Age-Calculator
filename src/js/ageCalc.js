5cd5434816f9a38070db601613be95113514b017
Richelle <richelle.ak@gmail.com>
2022-11-14 11:41:18 -0800

    pass first test for contructor properties

--- src/js/ageCalc.js
+++ src/js/ageCalc.js
@@ -1,5 +1,7 @@
 export default class AgeCalc {
   constructor(planet, userAge, userLifeExpectancy){
-
+    this.planet = planet;
+    this.userAge = userAge;
+    this.userLifeExpectancy= userLifeExpectancy;
   }
 }
