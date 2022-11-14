86d24eba2f1b53aa19ea3d159a9ac4965888947a
Richelle <richelle.ak@gmail.com>
2022-11-14 09:43:41 -0800

    test for object written - red

---
+++ __tests__/age.test.js
@@ -0,0 +1,12 @@
+import Age from  './../src/js/ageCalc.js';
+
+describe (`Age`, () => {
+  test(`should corect create an age object that contains multiple ages`, () => {
+    const ages =new Age(4,2,5,7,4);
+    expect(age.mercuryYrs).toEqual(4);
+    expect(age.venusYrs).toEqual(2);
+    expect(age.marsYrs).toEqual(5);
+    expect(age.jupiterYrs).toEqual(7);
+    expect(age.finalCountdown.toEqual(4);
+  });
+});
--- src/ageCalc.js
+++ src/ageCalc.js
@@ -1,10 +1,6 @@
 //this is test code
-function Triangle(side1, side2, side3) {
-  this.side1 = side1;
-  this.side2 = side2;
-  this.side3 = side3;
-}
+export default class Age {
+  constructor(mercuryYrs, venusYrs, marsYrs, jupiterYrs, finalCountdown){
 
-Triangle.prototype.checkType = function() {
-  return "I can't answer that yet!";
+  }
 }
