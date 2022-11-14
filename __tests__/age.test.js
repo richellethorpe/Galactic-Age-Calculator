634ee4be5a28d9eb012cbc40648af0b2311d5241
Richelle <richelle.ak@gmail.com>
2022-11-14 11:38:14 -0800

    update first RED test to find updated object contructor properities

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,8 +1,11 @@
-import Age from  './../src/js/ageCalc.js';
+import AgeCalc from  './../src/js/ageCalc.js';
 
-describe (`Age`, () => {
+describe (`AgeCalc`, () => {
   test(`should correctly create an age object that contains planet, user age, and life expectancy`, () => {
-
+    const ageCalc = new AgeCalc("earth", 36, 90)
+    expect(ageCalc.planet).toEqual("earth");
+    expect(ageCalc.userAge).toEqual(36);
+    expect(ageCalc.userLifeExpectancy).toEqual(90);
   });
 });
 
--- src/js/ageCalc.js
+++ src/js/ageCalc.js
@@ -1,7 +1,5 @@
-export default class Age {
+export default class AgeCalc {
   constructor(planet, userAge, userLifeExpectancy){
-    this.planet= planet;
-    this.userAge= userAge;
-    this.userLifeExpectancy = userLifeExpectancy;
+
   }
 }
