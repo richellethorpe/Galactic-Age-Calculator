fedba7e454dd3f04ae195204ab4b90cc88a78f85
Richelle <richelle.ak@gmail.com>
2022-11-14 11:09:01 -0800

    add passing test for first test GREEN

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,12 +1,12 @@
 import Age from  './../src/js/ageCalc.js';
 
 describe (`Age`, () => {
-  test(`should correctly create an age object that contains multiple ages`, () => {
+  test(`should correctly create an age object that contains 5 ages`, () => {
     const ages =new Age(4,2,5,7,4);
-    expect(age.mercuryYrs).toEqual(4);
-    expect(age.venusYrs).toEqual(2);
-    expect(age.marsYrs).toEqual(5);
-    expect(age.jupiterYrs).toEqual(7);
-    expect(age.finalCountdown).toEqual(4);
+    expect(ages.mercuryYrs).toEqual(4);
+    expect(ages.venusYrs).toEqual(2);
+    expect(ages.marsYrs).toEqual(5);
+    expect(ages.jupiterYrs).toEqual(7);
+    expect(ages.finalCountdown).toEqual(4);
   });
 });
--- src/js/ageCalc.js
+++ src/js/ageCalc.js
@@ -1,5 +1,9 @@
 export default class Age {
   constructor(mercuryYrs, venusYrs, marsYrs, jupiterYrs, finalCountdown){
-
+    this.mercuryYrs= mercuryYrs;
+    this.venusYrs= venusYrs;
+    this.marsYrs = marsYrs;
+    this.jupiterYrs=jupiterYrs;
+    this.finalCountdown= finalCountdown;
   }
 }
